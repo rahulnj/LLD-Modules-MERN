2ed86035c6a9f8e30ca772aeaf0c0ecad55cd060
rahulnj~ <rahuljayaraman8@gmail.com>
2024-08-25 00:23:41 +0530

    JS : flatten the array

---
+++ JS/array_flatten.js
@@ -0,0 +1,17 @@
+function flatten(array) {
+  let flattenedArray = [];
+
+  array.map((prop) => {
+    if (Array.isArray(prop)) {
+      const returnedArray = flatten(prop);
+      flattenedArray = [...flattenedArray, ...returnedArray];
+    } else {
+      flattenedArray.push(prop);
+    }
+  });
+
+  return flattenedArray;
+}
+
+const array = [1, 2, 3, [4, 5], [6, 7, 8, [9, 10, 11]]];
+console.log(flatten(array));
