855408f2d28d2b12e26368b880a884e078a96a1a
rahulnj~ <rahuljayaraman8@gmail.com>
2024-08-30 22:13:50 +0530

    JS : currying problems

--- JS/currying.js
+++ JS/currying.js
@@ -1,4 +1,5 @@
-function fn() {
+// Problem 1:
+function fn1() {
   let fnCallCount = 1;
   function innerFn(arg) {
     if (arg === 0) {
@@ -10,19 +11,55 @@ function fn() {
   return innerFn;
 }
 
-const func = fn();
-console.log(func(1)(1)(1)(0));
+const func1 = fn1();
+// console.log(func1(1)(1)(1)(0));
 
-// Lexical env
+// Problem 2: Lexical env
 var varName = 10;
 function b() {
   console.log(varName);
 }
-function fn() {
+function fn2() {
   var varName = 20;
   b();
   console.log(varName);
 }
-fn();
+fn2(); // Output : 10, 20
 
-// Output : 10, 20
+// Problem 3:
+function fn3() {
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  return function (a, b) {
+    if (b) {
+      return multiply(a, b);
+    } else {
+      return function (b) {
+        return multiply(a, b);
+      };
+    }
+  };
+}
+
+const func3 = fn3();
+// console.log(func3(1)(3));
+// console.log(func3(1, 3));
+
+// Problem 3: same as problem 2 but direct call without assigning to a variable
+function fn4(a, b) {
+  function multiply(a, b) {
+    return a * b;
+  }
+  if (b) {
+    return multiply(a, b);
+  } else {
+    return function (b) {
+      return multiply(a, b);
+    };
+  }
+}
+
+// console.log(fn4(1)(3));
+// console.log(fn4(1, 3));
