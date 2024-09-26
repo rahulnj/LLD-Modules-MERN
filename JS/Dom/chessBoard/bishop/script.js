f7346cf9c75572cef709a319af9ce6a7a4f6f7e7
rahulnj~ <rahuljayaraman8@gmail.com>
2024-09-26 21:55:33 +0530

    JS : DOM - bishop move

--- JS/Dom/chessBoard/bishop/script.js
+++ JS/Dom/chessBoard/bishop/script.js
@@ -1,55 +1,49 @@
-let checkBoxesDiv = document.querySelectorAll('div.checkbox');
-for (let i = 0; i < checkBoxesDiv.length; i++) {
-  checkBoxesDiv[i].addEventListener('click', () => {
-    boardRefresh();
-    traceBishopByIndex(i);
+const checkBoxesDiv = document.querySelectorAll('div.checkbox');
+const directions = [
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+];
+
+checkBoxesDiv.forEach((box, index) => {
+  box.addEventListener('click', () => {
+    refreshBoard();
+    highlightBishopPath(index);
+  });
+});
+
+function highlightBishopPath(index) {
+  turnCheckboxRed(index);
+
+  directions.forEach(([rowStep, colStep]) => {
+    let [row, col] = [Math.floor(index / 8), index % 8];
+
+    while (isInBounds(row + rowStep, col + colStep)) {
+      row += rowStep;
+      col += colStep;
+      turnCheckboxRed(row * 8 + col);
+    }
   });
 }
 
-function traceBishopByIndex(idx) {
-  let directionArr = [
-    [1, 1],
-    [-1, -1],
-    [-1, 1],
-    [1, -1],
-  ];
-
-  turnCheckboxRedByIndex(idx);
-  for (let di = 0; di < directionArr.length; di++) {
-    let dr = directionArr[di][0];
-    let dc = directionArr[di][1];
-
-    let ri = Math.floor(idx / 8);
-    let ci = idx - ri * 8;
-    // idx = (row * 8) + col + 1;
-    while (ri + dr < 8 && ri + dr >= 0 && ci + dc < 8 && ci + dc >= 0) {
-      ri = ri + dr;
-      ci = ci + dc;
-      turnCheckboxRedByIndex(ri * 8 + ci);
-    }
-  }
+function turnCheckboxRed(index) {
+  checkBoxesDiv[index].style.backgroundColor = 'red';
 }
 
-function turnCheckboxRedByIndex(idx) {
-  checkBoxesDiv[idx].style.backgroundColor = 'red';
+function refreshBoard() {
+  checkBoxesDiv.forEach((box) => {
+    const boxId = Number(box.id);
+    const rowId = Number(box.parentNode.id);
+
+    box.style.backgroundColor = getColorForCell(rowId, boxId);
+  });
 }
 
-function boardRefresh() {
-  for (let i = 0; i < checkBoxesDiv.length; i++) {
-    let boxId = Number(checkBoxesDiv[i]['id']);
-    let rowId = Number(checkBoxesDiv[i].parentNode.id);
-    if (rowId % 2 == 0) {
-      if (boxId % 2 != 0) {
-        checkBoxesDiv[i].style.backgroundColor = 'white';
-      } else {
-        checkBoxesDiv[i].style.backgroundColor = 'black';
-      }
-    } else {
-      if (boxId % 2 == 0) {
-        checkBoxesDiv[i].style.backgroundColor = 'white';
-      } else {
-        checkBoxesDiv[i].style.backgroundColor = 'black';
-      }
-    }
-  }
+function getColorForCell(row, col) {
+  return row % 2 === col % 2 ? 'black' : 'white';
+}
+
+function isInBounds(row, col) {
+  return row >= 0 && row < 8 && col >= 0 && col < 8;
 }
