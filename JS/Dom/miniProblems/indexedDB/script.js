9ad498764eafabd80d8c07b81be275d54a5a3ce5
rahulnj~ <rahuljayaraman8@gmail.com>
2024-10-06 10:18:47 +0530

    DOM : indexedDB script updated

--- JS/Dom/miniProblems/indexedDB/script.js
+++ JS/Dom/miniProblems/indexedDB/script.js
@@ -5,3 +5,45 @@ let address = document.querySelector('#add');
 let phone = document.querySelector('#ph');
 let btn = document.querySelector('button');
 let request = indexedDB.open('employees', 1);
+let userData = {};
+
+request.onsuccess = (e) => {
+  db = request.result;
+  console.log('Database opened successfully');
+};
+
+request.onerror = (e) => {
+  console.error('Database error:', e.target.errorCode);
+};
+
+request.onupgradeneeded = (e) => {
+  db = request.result;
+  db.createObjectStore('employee', { keyPath: 'eId' });
+  console.log('Object store created');
+};
+
+btn.addEventListener('click', () => {
+  addEmployee();
+});
+
+function addEmployee() {
+  let tx = db.transaction('employee', 'readwrite');
+  let store = tx.objectStore('employee');
+  let employeeData = {
+    eId: Date.now(),
+    name: name.value,
+    address: address.value,
+    phone: phone.value,
+  };
+
+  let request = store.add(employeeData);
+
+  request.onsuccess = () => {
+    console.log('Employee added to the database successfully');
+    console.log(employeeData);
+  };
+
+  request.onerror = (e) => {
+    console.error('Error adding employee to the database:', e.target.errorCode);
+  };
+}
