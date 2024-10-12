63c06df7a103f8cbb7e81e69f4b28b155e96ac5b
rahulnj~ <rahuljayaraman8@gmail.com>
2024-10-13 00:48:47 +0530

    DOM :  updated count down timer html boilerplate

--- JS/Dom/miniProblems/countDownTimer/script.js
+++ JS/Dom/miniProblems/countDownTimer/script.js
@@ -1,3 +1,6 @@
+const SECONDS_IN_MIN = 60;
+const SECONDS_IN_HOUR = SECONDS_IN_MIN * 60;
+
 const startBtn = document.getElementById('start');
 const resetBtn = document.getElementById('reset');
 const pauseBtn = document.getElementById('pause');
@@ -7,16 +10,36 @@ const hoursInput = document.getElementById('hr');
 const minutesInput = document.getElementById('min');
 const secondsInput = document.getElementById('sec');
 
-const SECONDS_IN_MIN = 60;
-const SECONDS_IN_HOUR = SECONDS_IN_MIN * 60;
-
 startBtn.addEventListener('click', () => {
   const hoursValue = getValidInput(hoursInput.value);
   const minutesValue = getValidInput(minutesInput.value);
   const secondsValue = getValidInput(secondsInput.value);
 
-  console.log(hoursValue);
+  if (!validateInputTime(hoursValue, minutesValue, secondsValue)) {
+    return;
+  }
+
+  const countDownTime =
+    hoursValue * SECONDS_IN_HOUR + minutesValue * SECONDS_IN_MIN + secondsValue;
 });
+
+function validateInputTime(hours, minutes, seconds) {
+  if (hours < 0 || minutes < 0 || seconds < 0) {
+    alert("Negative value's are not allowed.");
+    return false;
+  } else if (hours > 24) {
+    alert('Hour is greater than 24 which is not a valid hour.');
+    return false;
+  } else if (minutes > 60) {
+    alert('Minute is greater than 60 which is not a valid minute.');
+    return false;
+  } else if (seconds > 60) {
+    alert('Second is greater than 60 which is not a valid second.');
+    return false;
+  }
+
+  return true;
+}
 
 function getValidInput(value) {
   return parseInt(!value ? 0 : value);
