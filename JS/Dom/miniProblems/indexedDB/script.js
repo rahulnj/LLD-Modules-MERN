e8bfbf7a8795f3ac6f0e36c98d4a2cdbfab255a0
rahulnj~ <rahuljayaraman8@gmail.com>
2024-11-01 22:58:45 +0530

    JS : DOM : miniproblems-indexed db

--- JS/Dom/miniProblems/indexedDB/script.js
+++ JS/Dom/miniProblems/indexedDB/script.js
@@ -5,45 +5,31 @@ let address = document.querySelector('#add');
 let phone = document.querySelector('#ph');
 let btn = document.querySelector('button');
 let request = indexedDB.open('employees', 1);
-let userData = {};
 
-request.onsuccess = (e) => {
+request.onsuccess = function (e) {
   db = request.result;
-  console.log('Database opened successfully');
 };
 
-request.onerror = (e) => {
-  console.error('Database error:', e.target.errorCode);
+request.onerror = function (e) {
+  console.log(e);
 };
 
-request.onupgradeneeded = (e) => {
+request.onupgradeneeded = function (e) {
   db = request.result;
   db.createObjectStore('employee', { keyPath: 'eId' });
-  console.log('Object store created');
 };
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', function () {
   addEmployee();
 });
-
 function addEmployee() {
   let tx = db.transaction('employee', 'readwrite');
+
   let store = tx.objectStore('employee');
-  let employeeData = {
+  store.add({
     eId: Date.now(),
     name: name.value,
     address: address.value,
     phone: phone.value,
-  };
-
-  let request = store.add(employeeData);
-
-  request.onsuccess = () => {
-    console.log('Employee added to the database successfully');
-    console.log(employeeData);
-  };
-
-  request.onerror = (e) => {
-    console.error('Error adding employee to the database:', e.target.errorCode);
-  };
+  });
 }
