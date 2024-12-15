0a0fe0b2e82ccac14ed4b9d7c9afcd46032adb5f
rahulnj~ <rahuljayaraman8@gmail.com>
2024-12-15 22:34:44 +0530

    REACT : nested elements

--- REACT/reactScratch/app.js
+++ REACT/reactScratch/app.js
@@ -1,3 +1,10 @@
-const element = React.createElement('h1', { id: 'heading' }, 'Hello World');
+const element = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', { id: 'heading', key: 1 }, 'Hello World!'),
+    React.createElement('h1', { id: 'heading', key: 2 }, 'Hello World!'),
+  ])
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
