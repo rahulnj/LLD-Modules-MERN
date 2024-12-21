a6caf3159d474459b87e3d5cdeb08ced9ddba08b
rahulnj~ <rahuljayaraman8@gmail.com>
2024-12-21 23:52:02 +0530

    JS : using array prototype

--- JS/ArrayPolyfills/array_flatten.js
+++ JS/ArrayPolyfills/array_flatten.js
@@ -1,9 +1,10 @@
-function flatten(array) {
+Array.prototype.flatten = function () {
   let flattenedArray = [];
+  const array = this;
 
   array.forEach((prop) => {
     if (Array.isArray(prop)) {
-      const returnedArray = flatten(prop);
+      const returnedArray = prop.flatten();
       flattenedArray = [...flattenedArray, ...returnedArray];
     } else {
       flattenedArray.push(prop);
@@ -11,7 +12,7 @@ function flatten(array) {
   });
 
   return flattenedArray;
-}
+};
 
 const array = [1, 2, 3, [4, 5], [6, 7, 8, [9, 10, 11]]];
-console.log(flatten(array));
+console.log(array.flatten());
